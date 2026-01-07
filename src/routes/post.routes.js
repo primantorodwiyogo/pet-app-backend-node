4b218c5cf9b09b71b6921f00e5ab2d6c3bef50f0
primantorodwiyogo <primantoroo@outlook.com>
2026-01-08 04:54:18 +0700

    feat: add read-only post controller with relations

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,20 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
-const auth = require('../middleware/auth.middleware');
-const upload = require('../middleware/upload.middleware');
 const postController = require('../controllers/post.controller');
 
-router.get('/posts', postController.getPosts);
-router.get('/posts/mine', auth, postController.getMyPosts);
-router.get('/posts/:id', postController.getPostById);
-
-
-router.post(
-    '/posts',
-    auth,
-    upload.array('images', 5), // ⬅️ INI WAJIB
-    postController.createPost
-);
+router.get('/posts', postController.getAllPosts);
 
 module.exports = router;
